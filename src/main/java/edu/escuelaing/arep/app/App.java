ae5cae735b4f92094a86407bb0b25f119b0f4c36
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2021-02-19 14:15:16 -0500

    primera parte

---
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -0,0 +1,62 @@
+package edu.escuelaing.arep.app;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+import edu.escuelaing.arep.app.*;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.List;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+
+    /**
+     *El metodo mean permite iniciar el programa cuando este es ejecutado.
+     * @param args
+     */
+    public static void main(String[] args) {
+        staticFileLocation("/static");
+        port(getPort());
+        get("/trigonometrica",(req, res) -> calcula(req, res));
+
+    }
+
+    /**
+     * De acuerdo al tipo de operación trigonometrica retorna la respuesta-
+     * @param req
+     * @param res
+     * @return String que contiene la respuesta.
+     */
+    static String calcula(Request req, Response res){
+        String tipo = req.queryParams("tipo");
+        double num = Double.parseDouble(req.queryParams("num"));
+        Trigonometricas trigonometricas = new Trigonometricas();
+        double resultado = 0.0;
+        if(tipo.equals("cos")){
+            resultado = trigonometricas.getCos(num);
+        }else if(tipo.equals(("tan"))){
+            resultado = trigonometricas.getTan(num);
+        }else if(tipo.equals("sen"))
+            resultado = trigonometricas.getSen(num);
+
+        String mostrar = new Gson().toJson( "{\"Tu respuesta es\": \""+ resultado+"\"}");
+        return mostrar;
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 36500;
+    }
+
+
+
+}
---
+++ src/main/java/edu/escuelaing/arep/app/Trigonometricas.java
@@ -0,0 +1,24 @@
+package edu.escuelaing.arep.app;
+
+public class Trigonometricas {
+
+    public Trigonometricas() {
+    }
+
+    public double getTan(double num){
+        return Math.tan(num);
+    }
+
+    public double getCos(double num){
+        return Math.cos(num);
+    }
+
+    public double getSen(double num){
+        return Math.sin(num);
+    }
+
+
+
+
+
+}
